8ee2a9a4f7c50e01453f07cefe1442ec5699312c
terga17 <terglav10@gmail.com>
2024-11-07 16:50:49 +0100

    Skeleton za login/signup

---
+++ Frontend/PartyGamesWebHub/src/app/login-page/login-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { LoginFormComponent } from '../login-form/login-form.component';
+@Component({
+  selector: 'app-login-page',
+  standalone: true,
+  imports: [LoginFormComponent],
+  templateUrl: './login-page.component.html',
+  styleUrl: './login-page.component.css',
+})
+export class LoginPageComponent {}
