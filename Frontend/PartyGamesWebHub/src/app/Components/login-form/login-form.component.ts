29c95cad5766b023aaaeab33ce2a6dcbc8b95abf
terga17 <terglav10@gmail.com>
2024-11-17 17:20:01 +0100

    Login page form, output objekta v console

---
+++ Frontend/PartyGamesWebHub/src/app/Components/login-form/login-form.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-login-form',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './login-form.component.html',
+  styleUrl: './login-form.component.css',
+})
+export class LoginFormComponent {
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]],
+    });
+  }
+
+  get email() {
+    return this.loginForm.get('email');
+  }
+
+  get password() {
+    return this.loginForm.get('password');
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log('Form Submitted', this.loginForm.value);
+    } else {
+      console.log('Form is invalid');
+    }
+  }
+}
---
+++ Frontend/PartyGamesWebHub/src/app/Pages/hub-page/hub-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hub-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './hub-page.component.html',
+  styleUrl: './hub-page.component.css'
+})
+export class HubPageComponent {
+
+}
