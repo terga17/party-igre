c35a89b6a3af97cb15ef3a4c59de8d138187c543
terga17 <terglav10@gmail.com>
2024-11-25 21:47:48 +0100

    addes icons package

--- Frontend/PartyGamesWebHub/src/app/Pages/hub-page/hub-page.component.ts
+++ Frontend/PartyGamesWebHub/src/app/Pages/hub-page/hub-page.component.ts
@@ -1,16 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Router } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { fas } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-hub-page',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FontAwesomeModule],
   templateUrl: './hub-page.component.html',
   styleUrl: './hub-page.component.css',
 })
 export class HubPageComponent {
-  constructor(private router: Router) {}
+  constructor(private router: Router, library: FaIconLibrary) {
+    library.addIconPacks(fas);
+  }
   navigateToBuser() {
     this.router.navigate(['/hub']);
   }
