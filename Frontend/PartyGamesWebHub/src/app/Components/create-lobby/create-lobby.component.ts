06eaac48c799c609bc84b19ea9242637e5da732e
luka12333333 <luka.lamprecnik@student.um.si>
2024-12-05 18:12:22 +0100

    buser pre game lobby component
    html in css koda

---
+++ Frontend/PartyGamesWebHub/src/app/Components/create-lobby/create-lobby.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-lobby',
+  standalone: true,
+  imports: [],
+  templateUrl: './create-lobby.component.html',
+  styleUrl: './create-lobby.component.css'
+})
+export class CreateLobbyComponent {
+
+}
