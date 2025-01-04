ce33df6d357fd6b346049355740369877e89f3b1
ozzvaa <matevz.ozvatic@gmail.com>
2025-01-04 11:04:06 +0100

    Add games_table migration.

--- Backend/database/seeders/DatabaseSeeder.php
+++ Backend/database/seeders/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
-
+        $this->call(GamesTableSeeder::class);
         
         $u1 = User::create([
             'username' => 'Testni user1',
