350962ac1087b7c6f96b44d867ab052758ad0127
ozzvaa <matevz.ozvatic@gmail.com>
2025-01-06 13:36:09 +0100

    Add API comments

--- Frontend/PartyGamesWebHub/src/app/Services/api.service.ts
+++ Frontend/PartyGamesWebHub/src/app/Services/api.service.ts
@@ -14,43 +14,43 @@ export class ApiService {
     return this.http.get(`${this.apiUrl}/${endpoint}`);
   }
 
-  getUser(userId: string): Observable<any> {
+  getUser(userId: string): Observable<any> { // returns JSON object of a user
     return this.http.get(`${this.apiUrl}/user/${userId}`);
   }
 
-  getUsers(): Observable<any> {
+  getUsers(): Observable<any> { // returns JSON of all users : response.users
     return this.http.get(`${this.apiUrl}/user`);
   }
 
-  loginUser(data: { username: string; password: string }): Observable<any> {
+  loginUser(data: { username: string; password: string }): Observable<any> { // returns JSON of logged in user : response.user
     return this.http.post(`${this.apiUrl}/loginUser`, data);
   }
 
-  registerUser(data: { username: string; password: string }): Observable<any> {
+  registerUser(data: { username: string; password: string }): Observable<any> { // returns JSON of a registered user : response.user
     return this.http.post(`${this.apiUrl}/registerUser`, data);
   }
 
-  fetchFriends(userId: string): Observable<any> {
+  fetchFriends(userId: string): Observable<any> { // return JSON of users friends : response.user.friends
     return this.http.get(`${this.apiUrl}/user/${userId}/friends`);
   }
 
-  fetchPendingRequests(userId: string): Observable<any> {
+  fetchPendingRequests(userId: string): Observable<any> { // returns JSON of pending friend requests : response.pending_requests 
     return this.http.get(`${this.apiUrl}/user/${userId}/pending-friend-requests`);
   }
 
-  sendFriendRequest(userId: string, friendId: string): Observable<any> {
+  sendFriendRequest(userId: string, friendId: string): Observable<any> { // sends a friend request to another user
     return this.http.get(`${this.apiUrl}/user/${userId}/send-friend-request/${friendId}`);
   }
 
-  acceptFriendRequest(userId: string, requestId: string): Observable<any> {
+  acceptFriendRequest(userId: string, requestId: string): Observable<any> { // accepts a friend request based on request ID
     return this.http.get(`${this.apiUrl}/user/${userId}/accept-friend-request/${requestId}`);
   }
 
-  rejectFriendRequest(userId: string, requestId: string): Observable<any> {
+  rejectFriendRequest(userId: string, requestId: string): Observable<any> { // rejects a friend request based on request ID
     return this.http.get(`${this.apiUrl}/user/${userId}/reject-friend-request/${requestId}`);
   }
 
-  removeFriend(userId: string, friendId: string): Observable<any> {
+  removeFriend(userId: string, friendId: string): Observable<any> { // removes two friends
     return this.http.get(`${this.apiUrl}/user/${userId}/remove-friend/${friendId}`);
   }
   
