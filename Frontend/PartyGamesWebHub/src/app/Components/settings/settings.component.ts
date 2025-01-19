0c5b4cdf39e7d9015e4c98e3634b8f55d5a13134
ozzvaa <matevz.ozvatic@gmail.com>
2025-01-19 21:55:23 +0100

    Add pending requests showing

--- Frontend/PartyGamesWebHub/src/app/Components/settings/settings.component.ts
+++ Frontend/PartyGamesWebHub/src/app/Components/settings/settings.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, resolveForwardRef } from '@angular/core';
 import { ApiService } from '../../Services/api.service';
 import { UserService } from '../../Services/user.service';
 import { FormsModule } from '@angular/forms';
@@ -8,7 +8,7 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-settings',
   standalone: true,
-  imports: [FormsModule,CommonModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './settings.component.html',
   styleUrl: './settings.component.css',
 })
@@ -17,6 +17,7 @@ export class SettingsComponent {
   userId: string = '';
   friendList: any[] = [];
   allUsers: any[] = [];
+  friendRequests: any[] = [];
   constructor(
     private userService: UserService,
     private router: Router,
@@ -29,7 +30,7 @@ export class SettingsComponent {
     }
 
     this.displayFriends();
-
+    this.displayFriendRequests();
   }
 
   setting1: string = '';
@@ -63,6 +64,7 @@ export class SettingsComponent {
     // userId je ID prijavljenega uporabnika
     const userId = this.userService.getUserId();
     console.log("Searching for users friends...");
+    this.friendList = [];
     this.apiService.fetchFriends(userId).subscribe(
       (response) => {
         this.apiService.getUsers().subscribe(allUsers => {
@@ -71,7 +73,7 @@ export class SettingsComponent {
           console.log(response.user.friends)
           friends.forEach((friend: any) => {
             // Find the username of each friend by matching friend.id with the users' id
-            const matchedUser = allUsers.users.find((user:any) => user.id == friend.id);
+            const matchedUser = allUsers.users.find((user: any) => user.id == friend.id);
             if (matchedUser) {
               this.friendList.push(matchedUser); // Add the username to the array
             }
@@ -89,9 +91,45 @@ export class SettingsComponent {
     if (this.requestUsername != '') {
       alert(`${this.userName}, id: ${this.userId}\n sent friend request to: ${this.requestUsername}`);
     }
-    console.log(this.allUsers)
-    
+    this.apiService.sendFriendRequest(this.userId, this.requestUsername).subscribe(response => {
+      console.log(response);
+      this.displayFriends();
+    });
   }
 
+  removeFriend(username: string) {
+    alert(`Removed friend: ${username}`);
+    this.apiService.removeFriend(this.userId, username).subscribe(response => {
+      console.log(response);
+      this.displayFriends();
+    })
+  }
+
+  displayFriendRequests() {
+    this.apiService.fetchPendingRequests(this.userId).subscribe(response => {
+      const friendRequests = response.pending_requests;
+      console.log("Requests: ", friendRequests);
+      this.apiService.getUsers().subscribe((allUsers) => {
+        this.allUsers = allUsers.users;
+        console.log("Allusers req: ", allUsers.users);
+        friendRequests.forEach((fRequest: any) => {
+          const matchedUser = allUsers.users.find((user: any) => user.id == fRequest.sender_id);
+          if (matchedUser) {
+            console.log("Wants to be your friend: ", matchedUser); // Add the username to the array
+            this.friendRequests.push({"user": matchedUser, "request_id": fRequest.id});
+          }
+        })
+
+      })
+    })
+  }
+
+  acceptRequest(requestId: string) {
+    alert(`Accepted friend request!`);
+    this.apiService.acceptFriendRequest(this.userId, requestId).subscribe(response => {
+      console.log(response);
+      this.displayFriends();
+    });
+  }
 
 }
