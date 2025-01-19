254e309a09d5a075f5229e842f1e6fb64f223a22
terga17 <terglav10@gmail.com>
2025-01-19 21:02:53 +0100

    Malo style 1/2
    Nov style, hub page finished, malo warning odstraniti (nullables), dodan font in tictactoe slika igre

--- Frontend/PartyGamesWebHub/src/app/Pages/drive-the-bus/pyramid/pyramid.component.ts
+++ Frontend/PartyGamesWebHub/src/app/Pages/drive-the-bus/pyramid/pyramid.component.ts
@@ -11,23 +11,62 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [FontAwesomeModule, MatDialogModule, CommonModule],
   templateUrl: './pyramid.component.html',
-  styleUrl: './pyramid.component.css'
+  styleUrl: './pyramid.component.css',
 })
 export class PyramidComponent {
   private cardsList: string[] = [
-    '2_of_clubs.svg', '2_of_diamonds.svg', '2_of_hearts.svg', '2_of_spades.svg',
-    '3_of_clubs.svg', '3_of_diamonds.svg', '3_of_hearts.svg', '3_of_spades.svg',
-    '4_of_clubs.svg', '4_of_diamonds.svg', '4_of_hearts.svg', '4_of_spades.svg',
-    '5_of_clubs.svg', '5_of_diamonds.svg', '5_of_hearts.svg', '5_of_spades.svg',
-    '6_of_clubs.svg', '6_of_diamonds.svg', '6_of_hearts.svg', '6_of_spades.svg',
-    '7_of_clubs.svg', '7_of_diamonds.svg', '7_of_hearts.svg', '7_of_spades.svg',
-    '8_of_clubs.svg', '8_of_diamonds.svg', '8_of_hearts.svg', '8_of_spades.svg',
-    '9_of_clubs.svg', '9_of_diamonds.svg', '9_of_hearts.svg', '9_of_spades.svg',
-    '10_of_clubs.svg', '10_of_diamonds.svg', '10_of_hearts.svg', '10_of_spades.svg',
-    'ace_of_clubs.svg', 'ace_of_diamonds.svg', 'ace_of_hearts.svg', 'ace_of_spades2.svg',
-    'jack_of_clubs2.svg', 'jack_of_diamonds2.svg', 'jack_of_hearts2.svg', 'jack_of_spades2.svg',
-    'king_of_clubs2.svg', 'king_of_diamonds2.svg', 'king_of_hearts2.svg', 'king_of_spades2.svg',
-    'queen_of_clubs2.svg', 'queen_of_diamonds2.svg', 'queen_of_hearts2.svg', 'queen_of_spades2.svg',
+    '2_of_clubs.svg',
+    '2_of_diamonds.svg',
+    '2_of_hearts.svg',
+    '2_of_spades.svg',
+    '3_of_clubs.svg',
+    '3_of_diamonds.svg',
+    '3_of_hearts.svg',
+    '3_of_spades.svg',
+    '4_of_clubs.svg',
+    '4_of_diamonds.svg',
+    '4_of_hearts.svg',
+    '4_of_spades.svg',
+    '5_of_clubs.svg',
+    '5_of_diamonds.svg',
+    '5_of_hearts.svg',
+    '5_of_spades.svg',
+    '6_of_clubs.svg',
+    '6_of_diamonds.svg',
+    '6_of_hearts.svg',
+    '6_of_spades.svg',
+    '7_of_clubs.svg',
+    '7_of_diamonds.svg',
+    '7_of_hearts.svg',
+    '7_of_spades.svg',
+    '8_of_clubs.svg',
+    '8_of_diamonds.svg',
+    '8_of_hearts.svg',
+    '8_of_spades.svg',
+    '9_of_clubs.svg',
+    '9_of_diamonds.svg',
+    '9_of_hearts.svg',
+    '9_of_spades.svg',
+    '10_of_clubs.svg',
+    '10_of_diamonds.svg',
+    '10_of_hearts.svg',
+    '10_of_spades.svg',
+    'ace_of_clubs.svg',
+    'ace_of_diamonds.svg',
+    'ace_of_hearts.svg',
+    'ace_of_spades2.svg',
+    'jack_of_clubs2.svg',
+    'jack_of_diamonds2.svg',
+    'jack_of_hearts2.svg',
+    'jack_of_spades2.svg',
+    'king_of_clubs2.svg',
+    'king_of_diamonds2.svg',
+    'king_of_hearts2.svg',
+    'king_of_spades2.svg',
+    'queen_of_clubs2.svg',
+    'queen_of_diamonds2.svg',
+    'queen_of_hearts2.svg',
+    'queen_of_spades2.svg',
   ];
 
   cardBack: string = 'assets/Images/card-back-side.svg';
@@ -36,7 +75,11 @@ export class PyramidComponent {
   playerCards: { path: string; value: string }[][] = [];
   lastRevealedCard: string | null = null; // Beleži zadnjo obrnjeno karto na piramidi
 
-  constructor(private router: Router, library: FaIconLibrary, private dialog: MatDialog) {
+  constructor(
+    private router: Router,
+    library: FaIconLibrary,
+    private dialog: MatDialog
+  ) {
     library.addIconPacks(fas);
     this.initializeFlippedStates();
     this.assignCardValues();
@@ -81,15 +124,17 @@ export class PyramidComponent {
     if (!this.flippedStates[index]) {
       this.flippedStates[index] = true;
       this.lastRevealedCard = this.cards[index].value; // Nastavi zadnjo obrnjeno karto
-      console.log(`Karta na indeksu ${index} ima vrednost: ${this.cards[index].value}`);
+      console.log(
+        `Karta na indeksu ${index} ima vrednost: ${this.cards[index].value}`
+      );
     }
   }
 
-  checkPlayerCard(setIndex: number, cardIndex: number) {  
+  checkPlayerCard(setIndex: number, cardIndex: number) {
     const selectedCard = this.playerCards[setIndex][cardIndex];
     if (this.lastRevealedCard && selectedCard.value === this.lastRevealedCard) {
       console.log(`Karta ustreza: ${selectedCard.value}`);
-      this.playerCards[setIndex][cardIndex].path = this.cardBack; 
+      this.playerCards[setIndex][cardIndex].path = this.cardBack;
     } else {
       console.log('Karta ne ustreza.');
     }
